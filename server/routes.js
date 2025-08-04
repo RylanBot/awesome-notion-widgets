8650520968f7917263efeb9b1f8012de198f898f
Rylan <rylanbot@qq.com>
2025-08-04 14:38:43 +0800

    ♻️ refactor: update Bing image API fetching method

--- server/routes.js
+++ server/routes.js
@@ -8,6 +8,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const router = express.Router();
 
+/* ========== 页面 ========== */
+
 // 首页
 router.get("/", (_, res) => {
   res.sendFile(path.join(__dirname, "../views/index.html"));
@@ -26,12 +28,24 @@ router.get("/music", (_, res) => {
   res.sendFile(path.join(__dirname, "../views/widgets/music-player.html"));
 });
 
-// API
+/* ========== API ========== */
+
+// 随机引言
 router.get("/api/quote", (_, res) => {
   const quote = quotes.random();
   res.json({
     quote: quote.text,
     author: quote.author
+  });
+});
+
+// Bing 每日壁纸
+router.get("/api/bing-image", async (_, res) => {
+  const bingResponse = await fetch("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN");
+  const bingData = await bingResponse.json();
+  const bingImageUrl = bingData.images[0].url;
+  res.json({
+    url: `https://www.bing.com/${bingImageUrl}`
   });
 });
 
